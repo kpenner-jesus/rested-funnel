fd29732e850eb26048509de802fffab650179dab
kpenner-jesus <73081110+kpenner-jesus@users.noreply.github.com>
2026-03-14 19:17:48 -0500

    Update emailKeys.ts

--- app/emailKeys.ts
+++ app/emailKeys.ts
@@ -1,8 +1,8 @@
 export const EMAIL_KEYS = {
-  SERVICE_ID:        process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID        ?? "",
-  TEMPLATE_ID:       process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID       ?? "",
-  GUEST_TEMPLATE_ID: process.env.NEXT_PUBLIC_EMAILJS_GUEST_TEMPLATE_ID ?? "",
-  PUBLIC_KEY:        process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY        ?? "",
+  SERVICE_ID:        "service_z5vr2hr345",
+  TEMPLATE_ID:       "template_x5qjdb7t6",
+  GUEST_TEMPLATE_ID: "template_0sgecbx",
+  PUBLIC_KEY:        "Sr8ne3azvu5WL5xCk",
 };
 
 // TEMPORARY DEBUG
