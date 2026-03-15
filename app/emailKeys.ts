52a117b7a62622a56f675dc340dd4bca7410a537
kpenner-jesus <73081110+kpenner-jesus@users.noreply.github.com>
2026-03-14 21:30:13 -0500

    Update emailKeys.ts

--- app/emailKeys.ts
+++ app/emailKeys.ts
@@ -1,11 +1,6 @@
-import emailjs from "@emailjs/browser";
-
 export const EMAIL_KEYS = {
   SERVICE_ID:        "service_z5vr2hr345",
   TEMPLATE_ID:       "template_x5qjdb7t6",
   GUEST_TEMPLATE_ID: "template_0sgecbx",
   PUBLIC_KEY:        "Sr8ne3azvu5WL5xCk",
 };
-
-// Initialize EmailJS once when the module loads
-emailjs.init({ publicKey: EMAIL_KEYS.PUBLIC_KEY });
